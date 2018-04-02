ad097410a86c05038287fdd3fe219c5b4076428d
Lynn Langit <lynnlangit@live.com>
2018-04-02 08:56:13 -0700

    Improve readability of RDS instructions

--- 1a_RDS-MySQL-Nortwind/0_setup-rds.sh
+++ 1a_RDS-MySQL-Nortwind/0_setup-rds.sh
@@ -1,15 +1,17 @@
-# for demo setup single node dev edition RDS with MySQL all defaults and with public access
+# Set up RDS instance 
+# 1. Manually setup single node dev edition RDS with MySQL all defaults and with public access
 
-# download 14-day trial Navicat as client
+# Get Client
+# 2. download 14-day trial Navicat 
 # connect to instance endpoint, rather than cluster if using more than one node
 # connection string includes 1)endpoint 2)port <3306> 3)username 4)password
 
-# new query -> create a database
+# 3. Create Database via a 'new query...' in Navicat
 Create-Northwind.sql
 # refresh and switch to that database
 
-# new query -> load data
+# 4. Load Data via a 'new query...' in Navicat
 AddData-Northwind.sql
 
-# new query -> query data
+# 5. Query Data via a 'new query...' in Navicat'
 Query-Northwind.sql
