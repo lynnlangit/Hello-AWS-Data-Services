98322d688b7376e2372a1b426496664cffb128c7
Lynn Langit <lynnlangit@live.com>
2018-04-02 09:00:28 -0700

    Improve EMR readability instructions

--- 2_EMR-Spark/0_spark.sh
+++ 2_EMR-Spark/0_spark.sh
@@ -1,30 +1,33 @@
+# 0. Prepare
 # Create a key pair and download
-# Create a basic EMR cluster, associate the cluster with your new key pair
+
+# 1. Create a basic EMR cluster, associate the cluster with your new key pair
 # Set security group rule for SSH to allow your machine's IP address to connect to EMR master
 
+# 3. Prepare to connect 
 # Open terminal
 # Move (cd) to *.pem path
 # Set permissions to makes key publically viewable
 chmod 400 mykey.pem
 
-# Connect to EMR master node using SSH
+# 4. Connect to EMR master node using SSH
 # Update to reflect path to YOUR .pem key and YOUR EMR cluster endpoint (ip, etc...)
 ssh -i /Users/lynnlangit/Downloads/demo-emr.pem hadoop@ec2-34-224-6-68.compute-1.amazonaws.com
 
-# Start spark shell in SSH terminal window
+# 5. Start spark shell in SSH terminal window
 spark-shell
 
-# Verify spark context
+# 6. Verify spark context
 sc
 # OUTPUT: res0: org.apache.spark.SparkContext = org.apache.spark.SparkContext@404721db
 
-# Load text file
+# 7. Load text file
 val textFile = sc.textFile("s3://elasticmapreduce/samples/hive-ads/tables/impressions/dt=2009-04-13-08-05/ec2-0-51-75-39.amazon.com-2009-04-13-08-05.log")
 
-# Define the function
+# 8. Define the function
 val linesWithCartoonNetwork = textFile.filter(line => line.contains("cartoonnetwork.com")).count()
 # OUTPUT: linesWithCartoonNetwork: org.apache.spark.rdd.RDD[String] = MapPartitionsRDD[2] at filter at <console>:23
 
-# Exeute the function
+# 9. Exeute the function
 linesWithCartoonNetwork
 # OUTPUT: res2: Long = 9
