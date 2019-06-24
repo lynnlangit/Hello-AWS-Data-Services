e0eb2b3baea25d261280d7d997e1abe99b1b4139
Lynn Langit <lynnlangit@live.com>
2019-06-24 13:40:26 -0700

    Update s3.sh

--- 0a_S3/s3.sh
+++ 0a_S3/s3.sh
@@ -3,13 +3,17 @@ aws s3 mb <my-unique-bucket-name>
 
 # 1b. Tag a bucket
 aws s3api put-bucket-tagging --bucket <my-unique-bucket-name> --tagging file://tagging.json
+NOTE: **s3api** commands provide direct access to the S3 APIs & enable some operations that are not exposed in the high-level s3 commands
 
 # 2. Verify bucket was created by listing all buckets
 aws s3 ls
 
-# 3. Upload a file to your bucket
+# 3a. Upload a file to your bucket
 aws s3 cp /<my-path>/<my-file> s3://<my-unique-bucket-name>/
 EXAMPLE: aws s3 cp test.txt s3://mybucket/test2.txt
+
+# 3b. List the contents of your bucket
+$ aws s3 ls s3://<my-unique-bucket-name>
 
 # 4. Rename the file
 aws s3 –recursive mv s3://<my-unique-bucket-name>/<folder_name_from>/<old_file_name> \
