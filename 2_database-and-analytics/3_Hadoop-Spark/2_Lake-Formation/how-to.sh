ec1f4edf18c7cfa03196e31efcec08dd4a9f3734
Lynn Langit <lynnlangit@live.com>
2020-02-05 14:01:43 -0800

    added LakeFormation info

---
+++ 2_database-and-analytics/3_Hadoop-Spark/2_Lake-Formation/how-to.sh
@@ -0,0 +1,4 @@
+# Do prep steps
+# add a non root account admin user in IAM roles
+# add user as your AWS Lake admin - (beta) can associate to on prem Active Directory 
+# there is an example template at https://aws.amazon.com/solutions/data-lake-solution/
