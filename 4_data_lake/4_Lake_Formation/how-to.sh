c80365e990f085b69a8d9e74e30f50199f9bf0c0
Lynn Langit <lynnlangit@live.com>
2021-01-18 17:55:49 -0600

    Update how-to.sh

--- 4_data_lake/4_Lake_Formation/how-to.sh
+++ 4_data_lake/4_Lake_Formation/how-to.sh
@@ -1,3 +1,5 @@
 # Lake Formation
 
-- More info - https://aws.amazon.com/lake-formation
+- More info - https://aws.amazon.com/lake-formation    
+
+<img src="https://github.com/lynnlangit/Hello-AWS-Data-Services/blob/master/images/lake-formation.png" width=800>
