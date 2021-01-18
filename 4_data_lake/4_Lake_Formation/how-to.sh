340b9094d91692378864a258004ab34ce1617684
Lynn Langit <lynnlangit@live.com>
2021-01-18 17:54:17 -0600

    Update how-to.sh

--- 4_data_lake/4_Lake_Formation/how-to.sh
+++ 4_data_lake/4_Lake_Formation/how-to.sh
@@ -1,4 +1,3 @@
-# Do prep steps
-# add a non root account admin user in IAM roles
-# add user as your AWS Lake admin - (beta) can associate to on prem Active Directory 
-# there is an example template at https://aws.amazon.com/solutions/data-lake-solution/
+# Lake Formation
+
+- More info - https://aws.amazon.com/lake-formation
