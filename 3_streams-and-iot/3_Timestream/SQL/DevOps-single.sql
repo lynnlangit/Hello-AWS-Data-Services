1f39b305f58ecb89322219b9ef672004c0a1d1f1
Lynn Langit <lynnlangit@live.com>
2022-05-03 18:32:22 -0500

    Update DevOps-single.sql

--- 3_streams-and-iot/3_Timestream/SQL/DevOps-single.sql
+++ 3_streams-and-iot/3_Timestream/SQL/DevOps-single.sql
@@ -1,4 +1,4 @@
--- Find the average, p90, p95, and p99 CPU utilization for a specific EC2 host over the past 2 hours.
+-- 1. Find the average, p90, p95, and p99 CPU utilization for a specific EC2 host over the past 2 hours
 SELECT region, az, hostname, BIN(time, 15s) AS binned_timestamp,
     ROUND(AVG(measure_value::double), 2) AS avg_cpu_utilization,
     ROUND(APPROX_PERCENTILE(measure_value::double, 0.9), 2) AS p90_cpu_utilization,
@@ -11,7 +11,8 @@ WHERE measure_name = 'cpu_utilization'
 GROUP BY region, hostname, az, BIN(time, 15s)
 ORDER BY binned_timestamp ASC
 
--- Identify EC2 hosts with CPU utilization that is higher by 10 % or more compared to the average CPU utilization of the entire fleet for the past 2 hours.
+-- 2. Identify EC2 hosts with CPU utilization that is higher by 10 % or more compared 
+-- to the average CPU utilization of the entire fleet for the past 2 hours
 WITH avg_fleet_utilization AS (
     SELECT COUNT(DISTINCT hostname) AS total_host_count, AVG(measure_value::double) AS fleet_avg_cpu_utilization
     FROM "sampleDB".DevOps
@@ -29,7 +30,7 @@ FROM avg_fleet_utilization, avg_per_host_cpu
 WHERE avg_cpu_utilization > 1.1 * fleet_avg_cpu_utilization
 ORDER BY avg_cpu_utilization DESC
 
--- Find the average CPU utilization binned at 30 second intervals for a specific EC2 host over the past 2 hours.
+-- 3. Find the average CPU utilization binned at 30 second intervals for a specific EC2 host over the past 2 hours
 SELECT BIN(time, 30s) AS binned_timestamp, ROUND(AVG(measure_value::double), 2) AS avg_cpu_utilization
 FROM "sampleDB".DevOps
 WHERE measure_name = 'cpu_utilization'
